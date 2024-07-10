3b5827d6b308125bb2c80ff6a4a7c067890952e6
David Furey <david.furey@guardian.co.uk>
2024-07-10 13:53:55 +0100

    Override dymanodb name (temporary)

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,5 @@ export const STACK = process.env['STACK'] ?? "interactives";
 
 export const AWS_REGION = "eu-west-1";
 
-export const DYNAMODB_TABLE = `${STACK}-${STAGE}-${APP}`
+//export const DYNAMODB_TABLE = `${STACK}-${STAGE}-${APP}`
+export const DYNAMODB_TABLE = "interactives-CODE-gu-docs-TableCD117FA1-15P615BXL3FKE"
