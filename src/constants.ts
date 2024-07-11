b54a2d27345d34d85f02080b4634d389249e938a
David Furey <david.furey@guardian.co.uk>
2024-07-11 16:04:14 +0100

    Update db to correct name

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,4 @@ export const STACK = process.env['STACK'] ?? "interactives";
 
 export const AWS_REGION = "eu-west-1";
 
-//export const DYNAMODB_TABLE = `${STACK}-${STAGE}-${APP}`
-export const DYNAMODB_TABLE = "interactives-CODE-gu-docs-TableCD117FA1-15P615BXL3FKE"
+export const DYNAMODB_TABLE = `${STACK}-${STAGE}-${APP}`
