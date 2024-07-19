bc4c6d96a6dee6ae114ed4d2114aaf1921d70230
David Furey <david.furey@guardian.co.uk>
2024-07-19 18:11:14 +0100

    Tidy up LOCAL/CODE distinction
    Local will still use CODE database and S3

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,14 @@
 export const APP = "gudocs";
 
-export const STAGE = process.env['STAGE'] ?? "CODE";
+const stages = [ "PROD", "CODE", "LOCAL" ] as const;
+
+type Stage = typeof stages[number];
+
+export const STAGE: Stage = stages.find((stage) => stage === process.env['STAGE']) ?? "LOCAL";
 
 export const STACK = process.env['STACK'] ?? "interactives";
 
 export const AWS_REGION = "eu-west-1";
 
-export const DYNAMODB_TABLE = `${STACK}-${STAGE}-${APP}`
+export const DYNAMODB_TABLE = `${STACK}-${STAGE === "LOCAL" ? "CODE" : STAGE}-${APP}`
+
