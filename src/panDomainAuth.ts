450d8bffa2c7549590035760aac56bd401af3290
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2025-07-25 10:58:45 +0100

    Bump pan-domain-node

--- src/panDomainAuth.ts
+++ src/panDomainAuth.ts
@@ -1,26 +1,27 @@
 import {
-    AuthenticationStatus,
     guardianValidation,
     PanDomainAuthentication,
   } from "@guardian/pan-domain-node";
-  import { AWS_REGION, pandaPublicConfigFilename, pandaSettingsBucketName } from "./constants";
+import { standardAwsConfig } from "./awsIntegration";
+import { AWS_REGION, pandaPublicConfigFilename, pandaSettingsBucketName } from "./constants";
   
   const panda = new PanDomainAuthentication(
     "gutoolsAuth-assym", // cookie name
     AWS_REGION, // AWS region
     pandaSettingsBucketName, // Settings bucket
     pandaPublicConfigFilename, // Settings files
-    guardianValidation
+    guardianValidation,
+    standardAwsConfig.credentials,
   );
   
   export const getVerifiedUserEmail = async (
     cookieHeader: string | undefined
   ): Promise<void | string> => {
     if (typeof cookieHeader === "string") {
-      const { status, user } = await panda.verify(cookieHeader);
+      const result = await panda.verify(cookieHeader);
   
-      if (status === AuthenticationStatus.AUTHORISED && user !== undefined) {
-        return user.email;
+      if (result.success) {
+        return result.user.email;
       }
     }
   };
